f429a227fda0e995c238eea533fb41ccc72a2d80
Oz Mercer <Oz.Mercer@ig.com>
2020-07-10 00:49:57 +0100

    Add Welcome page & move nav-bar to separate component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TicTacToeComponent} from './game/tic-tac-toe.component';
-import {GameOfLifeComponent} from './game-of-life/game-of-life.component';
+import {TicTacToeComponent} from './games/game/tic-tac-toe.component';
+import {GameOfLifeComponent} from './games/game-of-life/game-of-life.component';
+import {WelcomePageComponent} from './games/welcome-page/welcome-page.component';
 
 
 const routes: Routes = [
   {path: 'tic-tac-toe', component: TicTacToeComponent},
   {path: 'game-of-life', component: GameOfLifeComponent},
+  {path: 'home', component: WelcomePageComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {TicTacToeComponent} from './game/tic-tac-toe.component';
-import {GameOfLifeComponent} from './game-of-life/game-of-life.component';
+import {TicTacToeComponent} from './games/game/tic-tac-toe.component';
+import {GameOfLifeComponent} from './games/game-of-life/game-of-life.component';
 import {FormsModule} from '@angular/forms';
-import {CellComponent} from './game-of-life/cell/cell.component';
+import {CellComponent} from './games/game-of-life/cell/cell.component';
+import { WelcomePageComponent } from './games/welcome-page/welcome-page.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import {CellComponent} from './game-of-life/cell/cell.component';
     TicTacToeComponent,
     GameOfLifeComponent,
     CellComponent,
+    WelcomePageComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
